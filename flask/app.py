1d13f336c08d0be259201f12565f92666486c9c5
mrgawde <mehulgawdenxt@gmail.com>
2018-02-20 00:54:44 +0530

    feat: Simple datastructure that can be accessed via GET method on a URI

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,40 @@
-#!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify, make_response, abort
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+#List of dictionaries
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+#index entry point we now have a get_tasks function that is associated with the /todo/api/v1.0/tasks URI, and only for the GET HTTP method.
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+#Return data of a single task
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+#Because our server should only be resonsing in json format we prevent the body to render a 404 template
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     app.run(debug=True)
