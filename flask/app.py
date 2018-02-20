c14f316d248b880e9545b18dadeb3db8a9c19511
mrgawde <mehulgawdenxt@gmail.com>
2018-02-20 16:55:10 +0530

    feat: Users can update and delete tasks

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, make_response, abort, request
-
 app = Flask(__name__)
 
 #List of dictionaries
@@ -51,6 +50,38 @@ def create_task():
     #Pass the data object into the data structure
     tasks.append(task)
     return jsonify({'task': task}), 201
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    #Look for task in tasks
+    task = [task for task in tasks if task['id'] == task_id]
+    #Checks if there exist any task
+    if len(task) == 0:
+        abort(404)
+    #Checks if request is of json type
+    if not request.json:
+        abort(400)
+    #Checks for title, description, done
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    #Updates values by overwriting
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
 
 
 if __name__ == '__main__':
